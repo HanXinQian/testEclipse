7e4b7d55270a60321c4dc924afc06f15522df2da
边边 <xinqian_han@outlook.com>
2019-06-12 15:15:14 +0800

    测试eclipse下的git使用

---
+++ src/main/java/com/hc/testgit/TestEclipseApplication.java
@@ -0,0 +1,13 @@
+package com.hc.testgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestEclipseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestEclipseApplication.class, args);
+	}
+
+}
